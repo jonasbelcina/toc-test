442cefec5d205ed15163d89af5987432e6a2219d
Jonas Belcina <jonas@elephantation.com>
2018-12-02 12:34:51 +0400

    add production enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  parseServerUrl: 'https://eyenight-dev.herokuapp.com/parse',
+  parseAppId: '1TcAvt0wD6YDxEffIJ7qJtRQvMXzu7',
+  parseClientKey: 'XUNfi612hLNdS6V8TTb582YTou8qSX'
 };
